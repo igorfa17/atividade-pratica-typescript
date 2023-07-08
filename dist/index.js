e264b7a43b6a33cb0a9517185943a330beea5be0
igorfa17 <igorfa17@gmail.com>
2023-07-08 11:19:17 -0300

    atividade 5 testada e funcionando

--- dist/index.js
+++ dist/index.js
@@ -112,3 +112,16 @@ lista.listarProdutos();
 lista.excluirProduto(2);
 // Listar produtos novamente
 lista.listarProdutos();
+// 5. 
+console.log('ATIVIDADE 5: ');
+function exibirInformacoesUsuario(user) {
+    const salario = user.salario ? `salário R$ ${user.salario}` : 'salário N/A';
+    console.log(`${user.nome}, ${user.idade} anos, ${user.ocupacao}, ${salario}`);
+}
+// Exemplo de uso
+const usuario1 = { nome: 'Daphne', idade: 23, ocupacao: 'analista de TI', salario: 1000 };
+const usuario2 = { nome: 'Carlos', idade: 30, ocupacao: 'engenheiro de software' };
+const usuario3 = { nome: 'Pedro', idade: 18, ocupacao: 'desenvolvedor full stack', salario: 3000 };
+exibirInformacoesUsuario(usuario1);
+exibirInformacoesUsuario(usuario2);
+exibirInformacoesUsuario(usuario3);
--- src/index.ts
+++ src/index.ts
@@ -165,4 +165,27 @@ interface Produto {
   lista.listarProdutos();
   
 // 5. 
+console.log('ATIVIDADE 5: ')
+interface User {
+    nome: string;
+    idade: number;
+    ocupacao: string;
+    salario?: number;
+  }
+  
+  function exibirInformacoesUsuario(user: User): void {
+    const salario = user.salario ? `salário R$ ${user.salario}` : 'salário N/A';
+    console.log(`${user.nome}, ${user.idade} anos, ${user.ocupacao}, ${salario}`);
+  }
+  
+  // Exemplo de uso
+  const usuario1: User = { nome: 'Daphne', idade: 23, ocupacao: 'analista de TI', salario: 1000 };
+  const usuario2: User = { nome: 'Carlos', idade: 30, ocupacao: 'engenheiro de software' };
+  const usuario3: User = { nome: 'Pedro', idade: 18, ocupacao: 'desenvolvedor full stack', salario: 3000 };
+
+  
+  exibirInformacoesUsuario(usuario1);
+  exibirInformacoesUsuario(usuario2);
+  exibirInformacoesUsuario(usuario3);
+
   
