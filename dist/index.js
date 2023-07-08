506e3f548e68d0218f92a31f4071b780b62c8672
igorfa17 <igorfa17@gmail.com>
2023-07-08 10:55:51 -0300

    atividade 2 testada

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+// 1. Crie uma função que receba 2 números e retorne um objeto contendo a média e também um indicador booleano de aprovado/reprovado. Considere aprovado com média >= 6.
 function calcularMedia(nota1, nota2) {
     const media = (nota1 + nota2) / 2;
     const situacao = media >= 6 ? 'Aprovado' : 'Reprovado';
@@ -9,3 +10,20 @@ function calcularMedia(nota1, nota2) {
 }
 const resultado = calcularMedia(2, 8);
 console.log(resultado);
+function calcularMediaPonderada(listaNotas) {
+    let somaPonderada = 0;
+    let somaPesos = 0;
+    for (const notaPeso of listaNotas) {
+        somaPonderada += notaPeso.nota * notaPeso.peso;
+        somaPesos += notaPeso.peso;
+    }
+    const mediaPonderada = somaPonderada / somaPesos;
+    return mediaPonderada;
+}
+// Exemplo de uso
+const listaExemplo = [
+    { nota: 7, peso: 2 },
+    { nota: 8, peso: 3 },
+];
+const mediaExemplo = calcularMediaPonderada(listaExemplo);
+console.log(mediaExemplo);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// 1. Crie uma função que receba 2 números e retorne um objeto contendo a média e também um indicador booleano de aprovado/reprovado. Considere aprovado com média >= 6.
+
 interface Resultado {
     media: number;
     situacao: string;
@@ -15,4 +17,33 @@ interface Resultado {
   
   const resultado = calcularMedia(2, 8);
   console.log(resultado);
+  
+// 2. Crie uma função que receba uma lista de objetos contendo nota e peso, realize a média das notas considerando o peso. Exemplos: Lista com 2 notas: (N1*P1) + (N2*P2) / 2 = Resultado // Lista com 3 notas: (N1*P1) + (N2*P2) + (N3*P3) / 3 = Resultado
+
+interface NotaPeso {
+    nota: number;
+    peso: number;
+  }
+  
+  function calcularMediaPonderada(listaNotas: NotaPeso[]): number {
+    let somaPonderada = 0;
+    let somaPesos = 0;
+  
+    for (const notaPeso of listaNotas) {
+      somaPonderada += notaPeso.nota * notaPeso.peso;
+      somaPesos += notaPeso.peso;
+    }
+  
+    const mediaPonderada = somaPonderada / somaPesos;
+    return mediaPonderada;
+  }
+  
+  // Exemplo de uso
+  const listaExemplo: NotaPeso[] = [
+    { nota: 7, peso: 2 },
+    { nota: 8, peso: 3 },
+  ];
+  
+  const mediaExemplo = calcularMediaPonderada(listaExemplo);
+  console.log(mediaExemplo);
   
